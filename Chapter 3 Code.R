691ca62cec5502193a3e7c09f657c9c73c516549
Infertile plant <lucymaywilson892003@gmail.com>
2025-01-28 11:51:15 +0000

    My First Commit (28.01.2025)

---
+++ Chapter 3 Code.R
@@ -0,0 +1,8 @@
+
+library(usethis) 
+install.packages(usethis)
+use_git_config(user.name  = "Infertile plant",
+               user.email = "lucymaywilson892003@gmail.com")
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+usethis::gh_token_help()
